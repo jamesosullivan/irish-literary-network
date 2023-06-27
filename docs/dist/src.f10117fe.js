2d2eef13c2b83ca9215d950761c0430d5fd9dcd7
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-06-27 15:57:58 -0400

    node_modules

--- docs/dist/src.f10117fe.js
+++ docs/dist/src.f10117fe.js
@@ -38792,7 +38792,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35189" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42409" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
